69c4484559b28f86fd49cb5033983dbd702978e6
JuanDiazCa <judiazca2@poligran.edu.co>
2021-11-13 14:13:35 -0500

    Crear controladores de CRUD de entidades

--- BackendEmpresa/src/controllers/index.ts
+++ BackendEmpresa/src/controllers/index.ts
@@ -3,3 +3,6 @@ export * from './empleado-empresa.controller';
 export * from './cliente-empresa.controller';
 export * from './empresa-cliente.controller';
 export * from './empresa-empleado.controller';
+export * from './empresa.controller';
+export * from './cliente.controller';
+export * from './empleado.controller';
