2e158570a424f8b4c76713fd3ac116abb281e2a7
JuanDiazCa <judiazca2@poligran.edu.co>
2021-11-29 20:15:41 -0500

    Proteger por estrategia de autorizacion a empresas

--- BackendEmpresa/src/controllers/empresa.controller.ts
+++ BackendEmpresa/src/controllers/empresa.controller.ts
@@ -1,3 +1,4 @@
+import { authenticate } from '@loopback/authentication';
 import {
   Count,
   CountSchema,
@@ -20,6 +21,7 @@ import {
 import {Empresa} from '../models';
 import {EmpresaRepository} from '../repositories';
 
+@authenticate("Empleado")
 export class EmpresaController {
   constructor(
     @repository(EmpresaRepository)
