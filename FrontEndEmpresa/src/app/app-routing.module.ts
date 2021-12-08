d6144e49c10523090dc9f5898956b6dd46b2f2d3
Angela <angmayorga@gmail.com>
2021-12-07 20:30:41 -0500

    se añade funcionalidades de cliente

--- FrontEndEmpresa/src/app/app-routing.module.ts
+++ FrontEndEmpresa/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AgregarClienteComponent } from './components/cliente/agregar-cliente/agregar-cliente.component';
 import { AgregarEmpleadoComponent } from './components/empleado/agregar-empleado/agregarEmpleado.component';
 import { LoginRegistroComponent } from './components/login-registro/login-registro.component';
 
 const routes: Routes = [
   { path: "", component: AppComponent, pathMatch: "full" },
   { path: "login", component: LoginRegistroComponent, pathMatch: "full" },
-  { path: "agregar-empleado", component: AgregarEmpleadoComponent, pathMatch: "full" }
+  { path: "agregar-empleado", component: AgregarEmpleadoComponent, pathMatch: "full" },
+  { path: "agregar-cliente", component: AgregarClienteComponent, pathMatch: "full" }
 ];
 
 @NgModule({
