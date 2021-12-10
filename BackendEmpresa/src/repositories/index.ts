c0b12cb1f477a28159f101b59980dca208fe03b0
JuanDiazCa <judiazca2@poligran.edu.co>
2021-12-10 03:16:50 -0500

    Repositorios pedido, producto, productoDePedido

--- BackendEmpresa/src/repositories/index.ts
+++ BackendEmpresa/src/repositories/index.ts
@@ -3,3 +3,6 @@ export * from './empleado.repository';
 export * from './empresa.repository';
 export * from './mensaje-empleado.repository';
 export * from './mensaje-cliente.repository';
+export * from './pedido.repository';
+export * from './producto.repository';
+export * from './productos-de-pedido.repository';
