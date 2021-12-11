546dc5534c193efd16fcf71b4d7d40b48df8fbdc
JuanDiazCa <judiazca2@poligran.edu.co>
2021-12-10 20:05:52 -0500

    Crear vista de perfil de empleado, perfil cliente

--- FrontEndEmpresa/src/app/components/perfiles/perfil-empleado/perfil-empleado.component.ts
+++ FrontEndEmpresa/src/app/components/perfiles/perfil-empleado/perfil-empleado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-perfil-empleado',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilEmpleadoComponent implements OnInit {
 
-  constructor() { }
+  editar: boolean = false;
+  cantidadPedidos:number=0;
+  fgValidador: FormGroup = this.fb.group({
+    'nombres': ['', [Validators.required]],
+    'apellidos': ['', [Validators.required]],
+    'direccion': ['', [Validators.required]],
+    'fechaNacimiento': ['', [Validators.required]],
+    'telefono': ['', [Validators.required]],
+    'correo': ['', [Validators.required, Validators.email]],
+    'sueldo': ['', [Validators.required, Validators.min(0)]],
+  }
+  );
+  subs: Subscription = new Subscription();
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+  Editar(){
+    this.editar=true;
+  }
+
+  GuardarCambios(){
+    //agregar logica de guardar
+    this.editar=false;
+  }
+  CancelarEditar(){
+    this.editar=false;
   }
 
 }
